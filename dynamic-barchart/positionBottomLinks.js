ac2480dae90432e97f92512bb6fe8f5fd5239a36
Jonathan Lin <jclin2013@gmail.com>
2017-10-03 23:43:23 -0400

    Add byline link

--- dynamic-barchart/positionBottomLinks.js
+++ dynamic-barchart/positionBottomLinks.js
@@ -1,7 +1,12 @@
-let xOffset = 5;
 let yOffset = h - 5;
 
 d3.select("#dataLinksContainer")
-  .attr("x", xOffset)
+  .attr("x", 5)
   .attr("y", yOffset)
   .attr("font-size", 10)
+
+d3.select("#bylineContainer")
+  .attr("x", w - 5)
+  .attr("y", yOffset)
+  .attr("font-size", 10)
+  .attr("text-anchor", "end")
