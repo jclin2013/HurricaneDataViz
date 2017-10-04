efab3deafa35908635d3be45dff8022afe4d4901
Jonathan Lin <jclin2013@gmail.com>
2017-10-03 22:49:28 -0400

    Add and configure tooltips to info icons

---
+++ dynamic-barchart/createInfoTooltips.js
@@ -0,0 +1,30 @@
+function displayTooltip(text, heightOfText) {
+  let tooltip = d3.select("#infoTooltip");
+  tooltip.classed("hidden", false);
+  tooltip.text(text);
+  let { left, top } = this.getBoundingClientRect();
+  tooltip.style("left", left + 15 + "px");
+  tooltip.style("top", top - heightOfText + "px");
+};
+
+let yearRangeText =
+  `Bucketize the data by different time intervals here.
+  When the time series data can't be divided into equal
+  durations, a 'remainder' year range will be produced,
+  but it will be excluded from trendline calculations.`;
+
+let include2017Text =
+  `Check the box to add the three category 4 hurricanes that
+  have made landfall in the U.S. so far in 2017.`;
+
+d3.select("#yearRangeInputContainer .fa-info-circle")
+  .on("mouseover", function() {
+    displayTooltip.bind(this)(yearRangeText, 170);
+  })
+  .on("mouseout", () => d3.select("#infoTooltip").classed("hidden", true));
+
+d3.select("#include2017CheckboxContainer .fa-info-circle")
+  .on("mouseover", function() {
+    displayTooltip.bind(this)(include2017Text, 95);
+  })
+  .on("mouseout", () => d3.select("#infoTooltip").classed("hidden", true));
