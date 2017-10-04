41529c1b4e7003e00f6b8bf48a4d93747bf5da58
Jonathan Lin <jclin2013@gmail.com>
2017-10-04 13:26:05 -0400

    Update links to include Economist viz

--- positionBottomLinks.js
+++ positionBottomLinks.js
@@ -3,10 +3,10 @@ let yOffset = h - 5;
 d3.select("#dataLinksContainer")
   .attr("x", 5)
   .attr("y", yOffset)
-  .attr("font-size", 10)
+  .attr("font-size", 9)
 
 d3.select("#bylineContainer")
   .attr("x", w - 5)
   .attr("y", yOffset)
-  .attr("font-size", 10)
+  .attr("font-size", 9)
   .attr("text-anchor", "end")
