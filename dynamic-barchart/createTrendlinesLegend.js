b6f4918ae72e44b365eb131e37e6719bb7387bd9
Jonathan Lin <jclin2013@gmail.com>
2017-10-04 12:22:54 -0400

    Add category labels to legend, recenter it

--- dynamic-barchart/createTrendlinesLegend.js
+++ dynamic-barchart/createTrendlinesLegend.js
@@ -1,19 +1,21 @@
 let createTrendlinesLegend = (svg, w, h, p) => {
   let legendTextY = h/5 - 25;
   let legendLineY = legendTextY - 5;
+  let legendX = w/9;
+
   //  trendlines label
   svg.append("text")
      .attr("y", legendTextY)
-     .attr("x", w/3 - 100)
+     .attr("x", legendX + 10)
      .text("TRENDLINES:")
      .attr("font-weight", "900");
 
   //all hurricanes line snippet
   svg.append("line")
       .attr("class", "trendlineAllHurricanes")
-      .attr("x1", w/3 + 17)
+      .attr("x1", legendX + 120)
       .attr("y1", legendLineY - 7)
-      .attr("x2", w/3 + 55)
+      .attr("x2", legendX + 160)
       .attr("y2", legendLineY - 7)
       .attr("stroke", "darkgreen")
       .attr("stroke-width", 4)
@@ -22,16 +24,16 @@ let createTrendlinesLegend = (svg, w, h, p) => {
   //  all hurricanes legend label
   svg.append("text")
      .attr("y", legendTextY - 7)
-     .attr("x", w/3 + 70)
+     .attr("x", legendX + 165)
      .attr("font-size", 12)
-     .text("All Hurricanes")
+     .text("All Hurricanes (Categories 1-5)")
 
   //major hurricanes line snippet
   svg.append("line")
       .attr("class", "trendlineMajorHurricanes")
-      .attr("x1", w/3 + 17)
+      .attr("x1", legendX + 120)
       .attr("y1", legendLineY + 7)
-      .attr("x2", w/3 + 55)
+      .attr("x2", legendX + 160)
       .attr("y2", legendLineY + 7)
       .attr("stroke", "#931319")
       .attr("stroke-width", 4)
@@ -40,15 +42,15 @@ let createTrendlinesLegend = (svg, w, h, p) => {
   //major hurricanes legend label
   svg.append("text")
     .attr("y", legendTextY + 7)
-    .attr("x", w/3 + 70)
+    .attr("x", legendX + 165)
     .attr("font-size", 12)
-    .text("Major Hurricanes")
+    .text("Major Hurricanes (Categories 3+)")
 
   //all hurricanes line slope
   svg.append("text")
      .attr("class", "allHurricanesLineSlope")
      .attr("y", legendTextY - 7)
-     .attr("x", w/3 + 175)
+     .attr("x", legendX + 360)
      .attr("font-size", 12)
      .text(allHurricanesLineSlope);
 
@@ -56,7 +58,7 @@ let createTrendlinesLegend = (svg, w, h, p) => {
   svg.append("text")
      .attr("class", "majorHurricanesLineSlope")
      .attr("y", legendTextY + 7)
-     .attr("x", w/3 + 175)
+     .attr("x", legendX + 360)
      .attr("font-size", 12)
      .text(majorHurricanesLineSlope);
 }
